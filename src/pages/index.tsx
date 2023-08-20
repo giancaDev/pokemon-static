835bf229ae1f0338dc20197260da0bb85312dd69
giancaH <giank.drums@gmail.com>
2023-08-20 17:38:01 -0500

    Add pokeApi and SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,41 @@
-import { Button } from '@nextui-org/react';
 import { Layout } from '@/components/layouts';
+import { GetStaticProps } from 'next';
+import pokeApi from '@/api/pokeApi';
+import { FC } from 'react';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
 
-export default function Page() {
+type PageProps = {
+  pokemons: SmallPokemon[];
+};
+
+const Page: FC<PageProps> = ({ pokemons }) => {
+  console.log('pokemons:', pokemons);
+
   return (
     <Layout title='Listado de pokemons'>
-      <Button color='gradient'>Click me</Button>
+      <ul>
+        {pokemons.map(({ name, id }) => (
+          <li key={id}>{`${id} - ${name}`}</li>
+        ))}
+      </ul>
     </Layout>
   );
-}
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
+};
+
+export default Page;
